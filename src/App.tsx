bca222e35385af90dacfd373dcf0c645102f72b1
Catalin-Andrei BORA <catalin.bora@gmail.com>
2021-06-26 18:50:09 +0300

    build fix ?

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import NavBar from "./components/organism/Navbar";
 import SignUpForm from "./components/organism/SignUp";
